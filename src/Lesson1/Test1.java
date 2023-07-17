f1283dd7d04251ac9fede966132e397330591a5a
yusuf-m-151 <yosuf.muradov.igra@mail.ru>
2023-07-17 23:41:01 +0400

    metod print and println

---
+++ src/Lesson1/Test1.java
@@ -0,0 +1,19 @@
+
+package Lesson1;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+       
+        System.out.print("А ");
+        System.out.println("a");
+        
+        System.out.print("B ");
+        System.out.println("b");
+        
+        System.out.print("C ");
+        System.out.println("c");
+        
+    }
+    
+}
